33086151d93f658d44d179936b5b186c0115fd13
evasilev <zhenya_vasilev@mail.ru>
2021-09-02 14:10:53 +0400

    Создан проект

--- README.md
+++
@@ -1,3 +0,0 @@
-# iOS
-
-Вся информация в [wiki](https://gitlab.study.htc-cs.com/root/ios/ios-21-1/-/wikis/home).
---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Евгений Васильев on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
