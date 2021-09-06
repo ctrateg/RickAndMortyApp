ed803b2f104fbd4cffe1b55ce00fdc9c2c34a193
evasilev <zhenya_vasilev@mail.ru>
2021-09-06 19:35:02 +0400

    Добавление доп файлов в репозиторий

--- RickAndMorty/RickAndMorty/Model/Utility.swift
+++ RickAndMorty/RickAndMorty/Model/Utility.swift
@@ -1,4 +1,17 @@
 import Foundation
+import Locksmith
 
 class Utility {
+}
+
+struct KeyChainAccount: CreateableSecureStorable, GenericPasswordSecureStorable {
+  let username: String
+  let password: String
+  var data: [String: Any] {
+    return ["password": password]
+  }
+  var service: String = "KeyChainAccount"
+  var account: String {
+    return username
+  }
 }
--- RickAndMorty/RickAndMorty/View/LoginView.swift
+++ RickAndMorty/RickAndMorty/View/LoginView.swift
@@ -1,33 +1,49 @@
-//
-//  Login.swift
-//  RickAndMorty
-//
-//  Created by Евгений Васильев on 03.09.2021.
-//
-
 import UIKit
+import Locksmith
 
 class Login: UIViewController {
   @IBOutlet weak var loginField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var loginButtonOutlet: UIButton!
-    @IBAction func loginButton(_ sender: Any) {
-  }
+  @IBOutlet weak var loginButtonOutlet: UIButton!
+    // toDo сделать проверку, прикрутить алерты на ошибку, изменить визуал текст филдов, отступ прикрутить
   let showPasswordButton = UIButton(type: .custom)
   let eyeImage = UIImage(systemName: "eye.fill")
+  let account = KeyChainAccount(username: "admin", password: "admin1")
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.navigationController?.isNavigationBarHidden = true
-    tabBarController?.tabBar.isHidden = true
+    try? account.createInSecureStore()
     loginButtonOutlet.layer.cornerRadius = 8
-    showPasswordButton.setImage(eyeImage, for: .normal)
-    showPasswordButton.addTarget(self, action: #selector(showPassword), for: .touchUpInside)
-    showPasswordButton.sizeToFit()
-    passwordField.isSecureTextEntry.toggle()
-    passwordField.rightView = showPasswordButton
-    passwordField.rightViewMode = .always
+    showPasswordButtonSetting()
+    passwordFieldButton()
+  }
+  @IBAction func loginButton(_ sender: Any) {
+    let dictLogin = Locksmith.loadDataForUserAccount(userAccount: "admin", inService: "KeyChainAccount")
+    if passwordField.text == dictLogin?["password"] as? String {
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      let mainTVC = storyboard.instantiateViewController(identifier: "MainInfoTableView")
+      mainTVC.loadViewIfNeeded()
+      mainTVC.modalPresentationStyle = .fullScreen
+      present(mainTVC, animated: true)
+    }
+  }
+  @IBAction func doneButtonLoginTF(_ sender: UITextField) {
+    sender.resignFirstResponder()
+  }
+  @IBAction func returnButtonPasswordTf(_ sender: UITextField) {
+    sender.resignFirstResponder()
   }
   @objc func showPassword() {
     passwordField.isSecureTextEntry.toggle()
+  }
+  func showPasswordButtonSetting() {
+    showPasswordButton.setImage(eyeImage, for: .normal)
+    showPasswordButton.addTarget(self, action: #selector(showPassword), for: .touchUpInside)
+    showPasswordButton.sizeToFit()
+    showPasswordButton.tintColor = .darkGray
+  }
+  func passwordFieldButton() {
+    passwordField.isSecureTextEntry.toggle()
+    passwordField.rightView = showPasswordButton
+    passwordField.rightViewMode = .always
   }
 }
