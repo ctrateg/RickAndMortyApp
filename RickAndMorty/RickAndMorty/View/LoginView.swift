de5a82b9c06f74afb26fdaf287be2f7ad515477c
evasilev <zhenya_vasilev@mail.ru>
2021-09-07 21:49:33 +0400

    Логин экран полностью готов: вывод ошибки, Лендскейп визуал и обычный, авторизация

--- RickAndMorty/RickAndMorty/View/LoginView.swift
+++ RickAndMorty/RickAndMorty/View/LoginView.swift
@@ -5,31 +5,57 @@ class Login: UIViewController {
   @IBOutlet weak var loginField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
   @IBOutlet weak var loginButtonOutlet: UIButton!
-    // toDo сделать проверку, прикрутить алерты на ошибку, изменить визуал текст филдов, отступ прикрутить
+  @IBOutlet weak var errorMessage: UILabel!
+    // toDo прикрутить алерты на ошибку
   let showPasswordButton = UIButton(type: .custom)
   let eyeImage = UIImage(systemName: "eye.fill")
   let account = KeyChainAccount(username: "admin", password: "admin1")
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.modalPresentationStyle = .fullScreen
+    errorMessage.isHidden = true
     try? account.createInSecureStore()
     loginButtonOutlet.layer.cornerRadius = 8
+    textFieldLine(textField: loginField)
+    textFieldLine(textField: passwordField)
     showPasswordButtonSetting()
     passwordFieldButton()
   }
-  @IBAction func loginButton(_ sender: Any) {
-    let dictLogin = Locksmith.loadDataForUserAccount(userAccount: "admin", inService: "KeyChainAccount")
+  func textFieldLine(textField: UITextField) {
+    let bottomLine = CALayer()
+    let borderWidth = CGFloat(1.0)
+    let hight = textField.frame.height
+    let width = textField.frame.width
+    bottomLine.frame = CGRect(x: 1, y: hight - borderWidth, width: width - 2, height: borderWidth)
+    bottomLine.backgroundColor = UIColor.lightGray.cgColor
+    textField.borderStyle = .none
+    bottomLine.borderWidth = borderWidth
+    textField.layer.addSublayer(bottomLine)
+    textField.layer.masksToBounds = true
+  }
+    @IBAction func loginClearField(_ sender: Any) {
+      loginField.text = ""
+      loginField.textColor = .black
+    }
+    @IBAction func passwordClearField(_ sender: Any) {
+      passwordField.text = ""
+      passwordField.textColor = .black
+    }
+    @IBAction func loginButton(_ sender: Any) {
+    let dictLogin = Locksmith.loadDataForUserAccount(userAccount: loginField.text ?? "", inService: "KeyChainAccount")
     if passwordField.text == dictLogin?["password"] as? String {
       let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      let mainTVC = storyboard.instantiateViewController(identifier: "MainInfoTableView")
+      let mainTVC = storyboard.instantiateViewController(identifier: "NavigationController")
       mainTVC.loadViewIfNeeded()
       mainTVC.modalPresentationStyle = .fullScreen
       present(mainTVC, animated: true)
+    } else {
+      errorMessage.isHidden = false
+      loginField.textColor = .red
+      passwordField.textColor = .red
     }
   }
   @IBAction func doneButtonLoginTF(_ sender: UITextField) {
-    sender.resignFirstResponder()
-  }
-  @IBAction func returnButtonPasswordTf(_ sender: UITextField) {
     sender.resignFirstResponder()
   }
   @objc func showPassword() {
@@ -42,7 +68,6 @@ class Login: UIViewController {
     showPasswordButton.tintColor = .darkGray
   }
   func passwordFieldButton() {
-    passwordField.isSecureTextEntry.toggle()
     passwordField.rightView = showPasswordButton
     passwordField.rightViewMode = .always
   }
--- RickAndMorty/RickAndMorty/View/PreviewViewController.swift
+++ RickAndMorty/RickAndMorty/View/PreviewViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 class PreviewViewController: UIViewController {
   @IBOutlet weak var previewImage: UIImageView!
   @IBOutlet weak var previewPCOutlet: UIPageControl!
-  @IBAction func previewPCAction(_ sender: Any) {
+  @IBAction func skipButton(_ sender: Any) {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let loginTVC = storyboard.instantiateViewController(identifier: "LoginStoryboard")
+    loginTVC.loadViewIfNeeded()
+    loginTVC.modalPresentationStyle = .fullScreen
+    present(loginTVC, animated: true)
+  }
+    @IBAction func previewPCAction(_ sender: Any) {
   previewImage.image = UIImage(named: "hello\(previewPCOutlet.currentPage + 1)")
   }
-
   override func viewDidLoad() {
   super.viewDidLoad()
   previewImage.image = UIImage(named: "hello1")
   // toDo пустые кнопки pagecontroller
-  // previewPageControl.customPageControl(dotFillColor: .white, dotBorderColor: UIColor(named: "MainColor") ?? .black, dotBorderWidth: 1)
   }
 }
